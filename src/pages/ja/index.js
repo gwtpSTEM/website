89cdeb94a6a2b529da93d7de4b29fe2bcb8e3843
Arisa Williams <arisa@gwtp.us>
2019-09-13 12:43:43 +0900

    update ja announcement

--- src/pages/ja/index.js
+++ src/pages/ja/index.js
@@ -100,7 +100,7 @@ export default ({ data }) => (
             <img src="https://source.unsplash.com/collection/8525904/500x300" className="card-img-top img-fluid" alt="" />
             <div className="card-body">
             <h3 className="card-title h5">東京・赤坂で英語STEM教室を開催！<br />(通訳同伴)</h3>
-            <p className="card-text">9月14日(土)暗号ブレスレットを作ろう<br />9月21日(土)オリジナルミニ懐中電灯を作ろう<br /><small className="text-danger font-weight-bold">場所: 港区赤坂4-2-3 赤坂コークス5階<br />時間: 午前9時半〜11時半<br />対象: 小学1年生〜6年生<br />費用: ¥5,000 (税・材料費込)</small></p>
+            <p className="card-text">9月21日(土)オリジナルミニ懐中電灯を作ろう<br /><small className="text-danger font-weight-bold">場所: 港区赤坂4-2-3 赤坂コークス5階<br />時間: 午前9時半〜11時半<br />対象: 小学1年生〜6年生<br />費用: ¥5,000 (税・材料費込)</small></p>
             <a href="https://airtable.com/shrnzDZ1VxVrNc1YO" className="btn btn-primary" aria-label="STEM教室の詳しい内容と登録フォームはこちら" target="_blank" rel="noopener noreferrer">参加申し込みフォームへ</a>
             </div>
           </div>
